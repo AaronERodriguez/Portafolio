90665814b7d652682ce0da50646beb3eec2a106c
Expak12 <69766780+Expak12@users.noreply.github.com>
2023-07-28 16:31:52 -0600

    init

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      textShadow: {
+        sm: '0 1px 2px var(--tw-shadow-color)',
+        DEFAULT: '0 2px 4px var(--tw-shadow-color)',
+        lg: '0 8px 16px var(--tw-shadow-color)',
+      },
+    },
+  },
+  plugins: [
+    require("flowbite/plugin"),
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
+  darkMode: 'class',
+}
+
