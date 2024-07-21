dfacfb95a0c0be724ce9c7cd65bc3d2b65c12fea
Expak12 <aarongt1808@gmail.com>
2024-07-20 20:30:44 -0600

    "Updated styles.css, index.html, and script.js files with various changes to layout, styling, and functionality."

--- script.js
+++ script.js
@@ -1,21 +1,36 @@
-const htmlSkill = document.getElementById('html');
-const cssSkill = document.getElementById('css');
-const jsSkill = document.getElementById('js');
-const reactSkill = document.getElementById('react');
-const pythonSkill = document.getElementById('python');
-const gitSkill = document.getElementById('git');
-const nodeSkill = document.getElementById('node');
-const sqlSkill = document.getElementById('sql');
-const skillsArr = [htmlSkill, cssSkill, jsSkill, reactSkill, pythonSkill, gitSkill, nodeSkill, sqlSkill];
+let htmlSkill = document.getElementById('html');
+let cssSkill = document.getElementById('css');
+let jsSkill = document.getElementById('js');
+let reactSkill = document.getElementById('react');
+let pythonSkill = document.getElementById('python');
+let gitSkill = document.getElementById('git');
+let skillsArr = [htmlSkill, cssSkill, jsSkill, reactSkill, pythonSkill, gitSkill];
+
+const tabs = document.querySelectorAll('.btn');
+const all_contents = document.querySelectorAll('.content');
+
+
+
+tabs.forEach((tab, index) => {
+    tab.addEventListener('click', () => {
+        tabs.forEach(tab => {
+            tab.classList.remove('active')
+        });
+        tab.classList.add('active');
+        
+        all_contents.forEach(content => {content.classList.remove('active')})
+        all_contents[index].classList.add('active')
+    })
+})
 
 const observer = new IntersectionObserver((entries) => {
-     entries.forEach((entry) => {
+    entries.forEach((entry) => {
         if (entry.isIntersecting) {
             entry.target.classList.add('show');
         } else {
             entry.target.classList.remove('show')
         }
-     })
+    })
 })
 
 const hiddenElements = document.querySelectorAll('.hide');
@@ -40,3 +55,12 @@ skillsArr.forEach(skill => {
         skillUnHoverHandler(event, skill.id)
     });
 })
+
+if (window.location.hash) {
+    const hash = window.location.hash;
+    const hashId = hash.replace('#', '');
+    const tab = document.getElementById(`${hashId}-btn`);
+    tab.click();
+} else {
+    console.log(tabs[0])
+}
